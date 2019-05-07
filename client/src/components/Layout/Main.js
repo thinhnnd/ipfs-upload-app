0d362d95caa516512f343c5fa7235a0d18d537b2
Thinh NND <16521179@gm.uit.edu.vn>
2019-05-07 10:14:56 +0700

    width of paper is larger

--- client/src/components/Layout/Main.js
+++ client/src/components/Layout/Main.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Header from './Header';
-import Footer from './Footer'
+import Footer from './Footer';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-const Main = ({ children }) => (
-    <div>
-        <Header />
-        <Grid container justify="center">
-            <Grid item xs={12} sm={6} style={{ marginTop: 30 }}>
-                {children}
-            </Grid>
-        </Grid>
-        <Footer />
-    </div>
-);
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
 
-export default Main;
+function Main(props) {
+    const {classes} = props;
+    return (
+        <div >
+            <Header />
+            <div className={classes.root}>
+                <Grid container justify="center" spacing={24}>
+                    <Grid item lg={10} xs={10} sm={10} style={{ marginTop: 30 }}>
+
+                        {props.children}
+                    </Grid>
+                </Grid>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
+
+Main.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+
+
+export default withStyles(styles)(Main);
